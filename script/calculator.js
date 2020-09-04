ebd516421f090ae2b461b4984095ce14165af443
pkunzel <paulo.kunzel@gmail.com>
2020-09-04 17:47:11 -0300

    a lot of stuff

--- script/calculator.js
+++ script/calculator.js
@@ -1,56 +1,69 @@
 class Calculator {
-    constructor(annualTax, annualInsurance, interestRate, loanAmount, yearsOfMortgage) {
-        Object.assign(this, { annualTax, annualInsurance, interestRate, loanAmount, yearsOfMortgage });
-    }
+	constructor(data) {
+		Object.assign(this, data);
+	}
 
-    /**
-     * @description Gets the monthly tax based on the annual value
-     * @returns {number} the monthly tax value
-     */
-    getTax = () => perMonth(this.annualTax.value);
+	/**
+	 * @description Gets the monthly tax based on the annual value
+	 * @returns {number} the monthly tax value
+	 */
+	getTax() {
+		return perMonth(this.annualTax.value);
+	}
 
-    /**
-     * @description Gets the monthly tax in Dollars
-     * @returns {string} A String with the monthly value masked in dollars
-     */
-    getTaxDollars = () => toDollar(this.getTax());
+	/**
+	 * @description Gets the monthly tax in Dollars
+	 * @returns {string} A String with the monthly value masked in dollars
+	 */
+	getTaxDollars() {
+		return toDollar(this.getTax());
+	}
 
-    /**
-     * @description Gets the Monthly Insurance based on the annual value
-     * @returns {number} the monthly insurance value
-     */
-    getInsurance = () => perMonth(this.annualInsurance.value);
+	/**
+	 * @description Gets the Monthly Insurance based on the annual value
+	 * @returns {number} the monthly insurance value
+	 */
+	getInsurance() {
+		return perMonth(this.annualInsurance.value);
+	}
 
-    /**
-     * @description Gets the monthly insurance in Dollars
-     * @returns {number} A String with the monthly value masked in dollars
-     */
-    getInsuranceDollars = () => toDollar(this.getInsurance());
+	/**
+	 * @description Gets the monthly insurance in Dollars
+	 * @returns {number} A String with the monthly value masked in dollars
+	 */
+	getInsuranceDollars() {
+		return toDollar(this.getInsurance());
+	}
 
-    /**
-     * @description Calculaters the Principle and interest based on the formula provided
-     * @returns {number} The Future Value divided by the interest over the period
-     */
-    getPrincipleAndInterest = () => {
-        const interestRate = this.interestRate.value;
-        const loanAmount = this.loanAmount.value;
-        const yearsOfMortgage = this.yearsOfMortgage.value;
+	/**
+	 * @description Calculaters the Principle and interest based on the formula provided
+	 * @returns {number} The Future Value divided by the interest over the period
+	 */
+	getPrincipleAndInterest() {
+		const interestRate = Number(this.interestRate.value);
+		const loanAmount = Number(this.loanAmount.value);
+		const yearsOfMortgage = Number(this.yearsOfMortgage.value);
 
-        const FutureValue = percentagePerMonth(interestRate) * loanAmount;
-        const InterestOverPeriod = (1 - Math.pow((1 + (percentagePerMonth(interestRate)), -yearsOfMortgage * 12)));
+		const FutureValue = percentagePerMonth(interestRate) * loanAmount;
 
-        return FutureValue / InterestOverPeriod;
-    };
+		const InterestOverPeriod = 1 - Math.pow(1 + percentagePerMonth(interestRate), -yearsOfMortgage * 12);
 
-    /**
-     * @description Gets Principle and interest masked in Dollars
-     * @returns {string} A String with the principle value masked in dollars
-     */
-    getPrincipleAndInterestDollars = () => toDollar(getPrincipleAndInterest);
+		return FutureValue / InterestOverPeriod;
+	}
 
-    /**
-     * @description Gets the monthly paymets masked in dollars
-     * @returns {string} A string containing the monthly payments masked in dollars
-     */
-    getMonthlyPayments = () => toDollar(getPrincipleAndInterest() + getTax() + getInsurance());
+	/**
+	 * @description Gets Principle and interest masked in Dollars
+	 * @returns {string} A String with the principle value masked in dollars
+	 */
+	getPrincipleAndInterestDollars() {
+		return toDollar(this.getPrincipleAndInterest());
+	}
+
+	/**
+	 * @description Gets the monthly paymets masked in dollars
+	 * @returns {string} A string containing the monthly payments masked in dollars
+	 */
+	getMonthlyPayments() {
+		return toDollar(this.getPrincipleAndInterest() + this.getTax() + this.getInsurance());
+	}
 }
--- script/main.js
+++ script/main.js
@@ -2,24 +2,58 @@
  * @description Function to be called when the Window Loads
  */
 function init() {
-    const calc = new Calculator(
-        $query("#annualTax"),
-        $query("#annualInsurance"),
-        $query("#interestRate"),
-        $query("#loanAmount"),
-        $query("#yearsOfMortgage")
-    )
+	$query("button").addEventListener("click", handleCalculate);
 
-    $query("button").addEventListener("click", calculatMortgage.bind(calc));
-
-};
-
-/**
- * @description Calculates the Mortgage data based on the provided values.
- */
-function calculatMortgage() {
-    console.log(this.getTaxDollars());
+	$queryAll('.inputRangeBlock input[type="range"]').forEach((element) => {
+		element.addEventListener("input", handleInputRange);
+	});
 }
 
+function handleInputRange(event) {
+	const inputRange = event.target;
+	const percentRange = (inputRange.value / inputRange.max) * 100;
+	inputRange.style.background = `linear-gradient(90deg, rgb(10,10,01) 0 ${percentRange}%, rgb(222,222,222) ${percentRange}%)`;
+	inputRange.nextElementSibling.nextElementSibling.value = inputRange.value;
+}
+
+function handleCalculate() {
+	const mortData = {
+		annualTax: $query("#annualTax"),
+		annualInsurance: $query("#annualInsurance"),
+		interestRate: $query("#interestRate"),
+		loanAmount: $query("#loanAmount"),
+		yearsOfMortgage: $query("#yearsOfMortgage"),
+	};
+
+	if (checkInputs([mortData.annualTax, mortData.annualInsurance, mortData.loanAmount])) {
+		const calc = new Calculator(mortData);
+
+		$query("#resultPrinciple").textContent = calc.getPrincipleAndInterestDollars();
+		$query("#resultTax").textContent = calc.getTaxDollars();
+		$query("#resultMonthlyPay").textContent = calc.getMonthlyPayments();
+	}
+}
+
+function checkInputs(checklist) {
+	let passed = true;
+	checklist.forEach((item) => {
+		if (item.value.trim() == "") {
+			passed = false;
+			showElementWithMessage(item.nextElementSibling, `${item.name} is mandatory`);
+		} else if (isNaN(item.value)) {
+			passed = false;
+			showElementWithMessage(item.nextElementSibling, "Value must be numeric");
+		} else {
+			showElementWithMessage(item.nextElementSibling, "");
+		}
+	});
+
+	return passed;
+}
+
+function showElementWithMessage(element, msg) {
+	element.style.borderColor = "red";
+	element.textContent = msg;
+}
 
 window.addEventListener("load", init);
